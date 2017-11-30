d3b5d80eea34366ebb2efea2ea175b8b9cb58a71
Dirk Reinemann <dirk.reinemann@gmx.de>
2017-11-30 18:37:08 +0100

    Updated cputemp script.

--- modules/cputemp.c
+++ modules/cputemp.c
@@ -10,76 +10,89 @@
 
 typedef struct {
     int temp;
-} Temp;
+} temp;
 
 typedef struct {
-    Temp *temps;
-    int   length;
-} Stat;
+    int   size;
+    temp *ts;
+} stat;
 
 const char *DIR_THERMAL = "/sys/class/thermal";
 const char *FILE_TEMP = "temp";
 const char *REGEX_THERMAL = "^thermal_zone";
 
-void read_stats(Stat *stat)
+void read_stats(stat *s)
 {
     DIR *dir = opendir(DIR_THERMAL);
-
-    stat->temps = malloc(sizeof(Temp));
 
     if (dir != NULL) {
         struct dirent *entry;
         regex_t regex;
         regcomp(&regex, REGEX_THERMAL, 0);
-        int i = 0;
         while ((entry = readdir(dir)) != NULL) {
             if (entry->d_type == DT_LNK && !regexec(&regex, entry->d_name, 0, NULL, 0)) {
-                char filepath[BUFSIZ];
+                size_t sfilepath = strlen(DIR_THERMAL) + strlen(entry->d_name) + strlen(FILE_TEMP) + 4;
+                char filepath[sfilepath];
                 snprintf(filepath, BUFSIZ - 1, "%s/%s/%s", DIR_THERMAL, entry->d_name, FILE_TEMP);
                 if (access(filepath, F_OK) != -1) {
                     FILE *file = fopen(filepath, "r");
                     if (file != NULL) {
-                        char *line;
-                        size_t size;
+                        char *line = NULL;
+                        size_t size = 0;
                         getline(&line, &size, file);
-                        int temp = atol(line) / 1000;
-                        free(line);
+                        if (line != NULL) {
+                            s->ts = (temp *)realloc(s->ts, (s->size + 1) * sizeof(temp));
+                            temp *t = s->ts + s->size;
+                            int temp = atol(line);
+                            if (temp > 1000)
+                                temp /= 1000;
+                            t->temp = temp;
+                            s->size++;
+                            free(line);
+                        }
                         fclose(file);
-
-                        if (i > 0)
-                            stat->temps = realloc(stat->temps, (i + 1) * sizeof(Stat));
-
-                        (stat->temps + i)->temp = temp;
-                        i++;
                     }
                 }
             }
         }
         closedir(dir);
-        stat->length = i;
     }
 }
 
 int main()
 {
-    Stat stat;
+    stat s;
 
-    read_stats(&stat);
-    char result[1024];
+    s.size = 0;
+    s.ts = (temp *)malloc(0 * sizeof(temp));
+
+    read_stats(&s);
+    char *result = (char *)malloc(1 * sizeof(char));
     result[0] = '\0';
-    int len = 0;
-    for (int i = 0; i < stat.length; i++) {
-        char temp[64];
-        size_t l = snprintf(temp, 64, "{\"processor\":\"processor %d\",\"temperature\":\"%d\"},", i + 1, (stat.temps + i)->temp);
-        strncpy(result + len, temp, l + 1);
-        len += l;
+
+    const char *format = "{\"processor\":\"processor %d\",\"temperature\":\"%d\"},";
+    size_t sformat = strlen(format) + 2;
+    size_t pos = 0;
+    for (int i = 0; i < s.size; i++) {
+        temp *t = s.ts + i;
+
+        char data[sformat];
+        snprintf(data, sformat, format, i + 1, t->temp);
+        size_t sdata = strlen(data);
+
+        result = (char *)realloc(result, (pos + sdata) * sizeof(char));
+        strncpy(result + pos, data, sdata);
+
+        pos += sdata;
     }
-    if (strlen(result) > 0) {
-        result[len - 1] = '\0';
+    result[pos - 1] = '\0';
+
+    if (strlen(result) > 0)
         printf("[%s]\n", result);
-    } else {
+    else
         printf("[]\n");
-    }
-    free(stat.temps);
+
+    free(result);
+    free(s.ts);
     return 0;
 }
